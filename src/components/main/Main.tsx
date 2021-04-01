a5fe855ef529acbf729990e08ceac2e0890073e0
Kaique Lima <kaique@leanwork.com.br>
2021-04-01 20:41:23 -0300

    21. First Component

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,7 +1,15 @@
+import * as S from './styles';
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>Typescript, ReactJS e Styled Components</S.Description>
+    <S.Illustration src="/img/hero-illustration.svg" alt="desenvolvedor " />
+  </S.Wrapper>
 );
 
 export default Main;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import GlobalStyles from 'styles/global';
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>E-commerce</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta name="description" content="A simple e-commerce" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default App;
